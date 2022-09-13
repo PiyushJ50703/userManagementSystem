715a5b540883717f9bb8b05aff87c23d267bc574
PiyushJ50703 <60563307+PiyushJ50703@users.noreply.github.com>
2022-09-13 18:00:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars'); // updated to 6.0.X
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: true})); // New
+
+// Parse application/json
+app.use(bodyParser.json());
+app.use(express.json()); // New
+
+// Static Files
+app.use(express.static('public'));
+
+
+// Update to 6.0.X
+const handlebars = exphbs.create({ extname: '.hbs',});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+ 
+const routes = require('./server/routes/user');
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
